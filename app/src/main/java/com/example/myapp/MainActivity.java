5bd6aac475b9d8bd533ae012353ef3ff172ae434
mr oh <dhtmdwns54@naver.com>
2023-01-19 16:32:13 +0900

    ver 1.5 code

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String readDay = null;
+    public String str = null;
+    private CalendarView calendarView;
+    private Button cha_Btn, del_Btn, save_Btn;
+    private TextView diaryTextView, textView2, textView3;
+    private EditText contextEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
